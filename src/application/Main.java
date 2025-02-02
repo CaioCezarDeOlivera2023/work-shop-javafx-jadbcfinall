cbb23f458f48be87a25825542cd250dc4b8dd92a
Caio Cezar <caio25m@gmail.com>
2025-02-02 13:57:04 -0300

    Main View

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,25 +1,27 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Main extends Application {
-	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));//aqui esta sendo instaciado o objeto FXMLLoader loader
+			Parent parent = loader.load();//aqui esta chamando o loader.load
+			Scene mainScene = new Scene(parent);//aqui esta sendo criado o objeto Scene passando como argumento o parent
+			primaryStage.setScene(mainScene);//aqui esta dando um set no palco principal
+			primaryStage.setTitle("Sample JavaFX application");//aqui esta dando um titulo.
+			primaryStage.show();//aqui vai mostrar o palco principal
+		} catch (IOException e) {//aqui esta tratando uma possivel exceção
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
