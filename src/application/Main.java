17fef886c98773405a277ff0659fbf1361a78b33
Caio Cezar <caio25m@gmail.com>
2025-02-03 11:08:15 -0300

    About View

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,9 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));//aqui esta sendo instaciado o objeto FXMLLoader loader
@@ -18,13 +21,17 @@ public class Main extends Application {
 			scrollPane.setFitToWidth(true);//aqui esta corrigindo a barra do painel para chegar até o final
 			
 			
-			Scene mainScene = new Scene(scrollPane);//aqui esta sendo criado o objeto Scene passando como argumento o parent
+			mainScene = new Scene(scrollPane);//aqui esta sendo criado o objeto Scene passando como argumento o parent
 			primaryStage.setScene(mainScene);//aqui esta dando um set no palco principal
 			primaryStage.setTitle("Sample JavaFX application");//aqui esta dando um titulo.
 			primaryStage.show();//aqui vai mostrar o palco principal
 		} catch (IOException e) {//aqui esta tratando uma possivel exceção
 			e.printStackTrace();
 		}
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 
 	public static void main(String[] args) {
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,13 +1,22 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
-public class MainViewController implements Initializable{//essa classe ira controlar o painel
+public class MainViewController implements Initializable {// essa classe ira controlar o painel
 
 	@FXML
 	private MenuItem menuItemSeller;
@@ -15,25 +24,44 @@ public class MainViewController implements Initializable{//essa classe ira contr
 	private MenuItem menuItemDepartment;
 	@FXML
 	private MenuItem menuitemAbout;
-	
+
 	@FXML
-	public void onMenuItemSellerAction() {//esse metho ira tratar os eventos do seller
+	public void onMenuItemSellerAction() {// esse metho ira tratar os eventos do seller
 		System.out.println("onMenuItemSellerAction");
 	}
+
 	@FXML
-	public void onMenuItemDepartmentAction() {//esse metodo ira tratar os eventos do department
+	public void onMenuItemDepartmentAction() {// esse metodo ira tratar os eventos do department
 		System.out.println("onMenuItemDepartmentAction");
 	}
+
 	@FXML
-	public void onMenuItemAboutAction() {//esse metodo ira tratar os eventos do about
-		System.out.println("onMenuItemAboutAction");
-	}
-	
-	
-	@Override
-	public void initialize(URL uri, ResourceBundle rb) {//esse metodo é da interface Initializable
-		
-		
+	public void onMenuItemAboutAction() {// esse metodo ira tratar os eventos do about
+		loadView("/gui/About.fxml");
 	}
 
+	@Override
+	public void initialize(URL uri, ResourceBundle rb) {// esse metodo é da interface Initializable
+	}
+
+	private synchronized void loadView(String absoluteName) {//synchronized esta garantindo que a aplicação não tenha nenhuma interrupção
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));// aqui esta carregando a view about
+			VBox newVBox = loader.load();
+			
+			//aqui esta sendo manipulado os eventos do About view
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox)((ScrollPane)mainScene.getRoot()).getContent();
+			
+			
+			Node mainManu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainManu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+		}
+		catch (IOException e) {//aqui esta sendo tratado uma possivel exceção
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
 }
